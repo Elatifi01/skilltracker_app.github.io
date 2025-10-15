1683355995d7d6d2304a34a00b77c45cb99697d0
Haitam El Atifi <elatifi.haitam01@gmail.com>
2025-10-15 03:38:09 -0700

    Fix : Database

--- skilltracker/settings.py
+++ skilltracker/settings.py
@@ -73,13 +73,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'skilltracker.wsgi.application'
 
-
+import dj_database_url
+import os
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",  # fallback for local dev
+        conn_max_age=600,
+    )
 }
+# For local development, you can uncomment the following DATABASES setting
+# and comment out the dj_database_url part above.
+#   DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.sqlite3',
+#           'NAME': BASE_DIR / 'db.sqlite3',
+#       }
+#   }   
+# End of local development database settings
 
 
 
